98ac5565847824854405171002ad6a7238a331eb
bmuday <bmuday@live.fr>
2021-07-21 14:32:20 +0200

    add controllers

--- routes/posts.js
+++ routes/posts.js
@@ -1,33 +1,9 @@
 const router = require("express").Router();
-const Post = require("../models/Post");
+const { getPosts, newPost } = require("../controllers/post");
 
 // ROUTES
-router.get("/", (req, res) => {
-  Post.find({})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
-});
+router.get("/", getPosts);
 
-router.post("/save", (req, res) => {
-  //console.log(`Body: ${req.body}`);
-  const data = req.body;
-  const newPost = new Post(data);
-
-  newPost.save((err) => {
-    if (err) {
-      res.status(500).json({
-        msg: "Sorry, your data has not been saved due to an internal server error.",
-      });
-      return;
-    }
-    return res.json({
-      msg: "We received your data!",
-    }); // by default, .status(200) = success
-  });
-});
+router.post("/save", newPost);
 
 module.exports = router;
